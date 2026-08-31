c06e1e864f96f48bbaac71839bddb6a69759accc
Kidus <kidusg79@gmail.com>
2026-08-31 09:31:34 +0300

    Build static Addis Eats React menu

---
+++ day_26/addis-eats/src/App.jsx
@@ -0,0 +1,45 @@
+import Header from "./Header";
+import Dish from "./Dish";
+
+const menu = [
+  {
+    id: 1,
+    name: "Doro Wat",
+    price: 240,
+  },
+  {
+    id: 2,
+    name: "Shiro",
+    price: 120,
+  },
+  {
+    id: 3,
+    name: "Tibs",
+    price: 280,
+  },
+  {
+    id: 4,
+    name: "Kitfo",
+    price: 350,
+  },
+];
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+
+      <main>
+        <h2>Our Menu</h2>
+
+        <div className="menu">
+          {menu.map((dish) => (
+            <Dish key={dish.id} name={dish.name} price={dish.price} />
+          ))}
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ day_26/addis-eats/src/Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+  return (
+    <header>
+      <h1>Addis Eats</h1>
+      <p>Delicious Ethiopian food in Addis Ababa</p>
+    </header>
+  );
+}
+
+export default Header;
